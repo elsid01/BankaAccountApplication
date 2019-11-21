70e4b27353250b83a44a31ad4c19d30c646af3b2
elsid01 <50616793+elsid01@users.noreply.github.com>
2019-11-21 16:52:03 -0500

    Add files via upload

---
+++ AccountDAOI.java
@@ -0,0 +1,12 @@
+package adj01AccountsCRUD.Version3;
+
+public interface AccountDAOI {
+	
+	public void addAccount(Account account);
+	public void updateAccount(int index, Account account);
+	public void deleteAccount(int index);
+	public Account showAccount(int index);
+	public int findAccount(Account account);
+	public boolean isAccountsEmpty();
+	public boolean isAccountsFull();
+}
